bf23aaf89d858959d5131999634dfc036650b182
plinss@d4b6719f-082f-4061-9c97-831ddc045d98 <plinss@d4b6719f-082f-4061-9c97-831ddc045d98>
2010-10-18 18:14:28 +0000

    remove unnecessary data from results table, reduce field sizes

--- lib_css2.1_harness/css2test.sql
+++ lib_css2.1_harness/css2test.sql
@@ -24,11 +24,8 @@ CREATE TABLE IF NOT EXISTS `flags` (
 CREATE TABLE IF NOT EXISTS `results` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `testcase_id` int(11) unsigned NOT NULL default '0',
-  `testsuite` varchar(255) NOT NULL default '',
-  `testcase` varchar(255) NOT NULL default '',
-  `useragent` varchar(255) NOT NULL default '',
   `useragent_id` int(11) unsigned NOT NULL default '0',
-  `source` varchar(15) NULL default '',
+  `source` varchar(16) NULL default '',
   `result` enum('pass','fail','uncertain','na') NOT NULL default 'pass',
   `modified` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`)
@@ -45,12 +42,12 @@ CREATE TABLE IF NOT EXISTS `results` (
 CREATE TABLE IF NOT EXISTS `testcases` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `uri` varchar(255) NOT NULL default '',
-  `testsuite` varchar(255) NOT NULL default '',
-  `testcase` varchar(255) NOT NULL default '',
+  `testsuite` varchar(32) NOT NULL default '',
+  `testcase` varchar(64) NOT NULL default '',
   `title` varchar(255) default NULL,
   `flags` set('ahem','dom','font','history','HTMLonly','image','interact','invalid','namespace','nonHTML','may','paged','should','scroll','svg','96dpi') default NULL,
   `assertion` varchar(255) default NULL,
-  `testgroup` varchar(255) default NULL,
+  `testgroup` varchar(32) default NULL,
   `modified` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -74,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `testgroups` (
 --
 
 CREATE TABLE IF NOT EXISTS `testsuites` (
-  `testsuite` varchar(255) NOT NULL default '',
+  `testsuite` varchar(32) NOT NULL default '',
   `uri` varchar(255) default NULL,
   `title` varchar(255) default NULL,
   `description` longtext,
@@ -90,10 +87,10 @@ CREATE TABLE IF NOT EXISTS `testsuites` (
 CREATE TABLE IF NOT EXISTS `useragents` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `useragent` varchar(255) NOT NULL default '',
-  `engine` varchar(255) default NULL,
-  `engine_version` varchar(255) default NULL,
-  `browser` varchar(255) default NULL,
-  `browser_version` varchar(255) default NULL,
-  `platform` varchar(255) default NULL,
+  `engine` varchar(16) default NULL,
+  `engine_version` varchar(16) default NULL,
+  `browser` varchar(32) default NULL,
+  `browser_version` varchar(16) default NULL,
+  `platform` varchar(32) default NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
