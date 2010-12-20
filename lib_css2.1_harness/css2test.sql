1ea3d2d3afb16448b2dbce1736e02a89d049da69
plinss@d4b6719f-082f-4061-9c97-831ddc045d98 <plinss@d4b6719f-082f-4061-9c97-831ddc045d98>
2010-12-20 20:31:21 +0000

    update db table definition, spidertrap store entire url

--- lib_css2.1_harness/css2test.sql
+++ lib_css2.1_harness/css2test.sql
@@ -10,10 +10,10 @@
 --
 
 CREATE TABLE IF NOT EXISTS `flags` (
-  `flag` enum('ahem','dom','font','history','HTMLonly','image','interact','invalid','namespace','nonHTML','may','paged','reftest','should','scroll','svg','96dpi') collate utf8_bin NOT NULL default 'ahem',
-  `description` longtext collate utf8_bin NOT NULL,
+  `flag` enum('ahem','animated','dom','font','history','http','HTMLonly','image','interact','invalid','namespace','nonHTML','may','paged','reftest','should','scroll','svg','userstyle','96dpi') NOT NULL default 'ahem',
+  `description` varchar(255) NOT NULL,
   PRIMARY KEY  (`flag`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -24,11 +24,11 @@ CREATE TABLE IF NOT EXISTS `flags` (
 CREATE TABLE IF NOT EXISTS `reftests` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `testcase_id` int(11) unsigned NOT NULL,
-  `reference` varchar(255) collate utf8_bin NOT NULL,
-  `uri` varchar(255) collate utf8_bin NOT NULL,
-  `type` enum('==','!=') collate utf8_bin NOT NULL,
+  `reference` varchar(255) NOT NULL,
+  `uri` varchar(255) NOT NULL,
+  `type` enum('==','!=') NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -40,14 +40,15 @@ CREATE TABLE IF NOT EXISTS `results` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `testcase_id` int(11) unsigned NOT NULL default '0',
   `useragent_id` int(11) unsigned NOT NULL default '0',
-  `source` varchar(16) collate utf8_bin default NULL,
+  `source` varchar(16) default NULL,
   `original_id` int(11) unsigned default NULL,
-  `result` enum('pass','fail','uncertain','na','invalid') collate utf8_bin NOT NULL default 'na',
+  `result` enum('pass','fail','uncertain','na','invalid') NOT NULL default 'na',
+  `ignore` int(1) unsigned NOT NULL,
   `modified` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`),
   KEY `useragent_id` (`useragent_id`),
   KEY `testcase_id` (`testcase_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -56,15 +57,15 @@ CREATE TABLE IF NOT EXISTS `results` (
 --
 
 CREATE TABLE IF NOT EXISTS `spidertrap` (
-  `ip_address` varchar(15) collate utf8_bin NOT NULL,
-  `user_agent` varchar(255) collate utf8_bin default NULL,
-  `last_query` varchar(255) collate utf8_bin default NULL,
+  `ip_address` varchar(15) NOT NULL,
+  `user_agent` varchar(255) default NULL,
+  `last_uri` varchar(255) default NULL,
   `visit_count` int(11) NOT NULL default '0',
   `banned` tinyint(1) NOT NULL default '0',
   `first_visit` timestamp NULL default NULL,
   `last_action` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`ip_address`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -74,18 +75,18 @@ CREATE TABLE IF NOT EXISTS `spidertrap` (
 
 CREATE TABLE IF NOT EXISTS `testcases` (
   `id` int(11) unsigned NOT NULL auto_increment,
-  `uri` varchar(255) collate utf8_bin NOT NULL default '',
-  `testsuite` varchar(32) collate utf8_bin NOT NULL default '',
-  `testcase` varchar(64) collate utf8_bin NOT NULL default '',
-  `title` varchar(255) collate utf8_bin default NULL,
-  `flags` set('ahem','dom','font','history','HTMLonly','image','interact','invalid','namespace','nonHTML','may','paged','reftest','should','scroll','svg','96dpi') collate utf8_bin default NULL,
-  `assertion` varchar(255) collate utf8_bin default NULL,
-  `testgroup` varchar(32) collate utf8_bin default NULL,
+  `uri` varchar(255) NOT NULL default '',
+  `testsuite` varchar(32) NOT NULL default '',
+  `testcase` varchar(64) NOT NULL default '',
+  `title` varchar(255) default NULL,
+  `flags` set('ahem','animated','dom','font','history','http','HTMLonly','image','interact','invalid','namespace','nonHTML','may','paged','reftest','should','scroll','svg','userstyle','96dpi') default NULL,
+  `assertion` varchar(255) default NULL,
+  `testgroup` varchar(32) default NULL,
   `grandfather` tinyint(1) NOT NULL default '0',
   `active` tinyint(1) NOT NULL default '1',
   `modified` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -95,10 +96,10 @@ CREATE TABLE IF NOT EXISTS `testcases` (
 
 CREATE TABLE IF NOT EXISTS `testgroups` (
   `id` int(11) unsigned NOT NULL auto_increment,
-  `testgroup` varchar(32) collate utf8_bin NOT NULL default '',
-  `title` varchar(255) collate utf8_bin NOT NULL default '',
+  `testgroup` varchar(32) NOT NULL default '',
+  `title` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -108,10 +109,10 @@ CREATE TABLE IF NOT EXISTS `testgroups` (
 
 CREATE TABLE IF NOT EXISTS `testlinks` (
   `testcase_id` int(11) unsigned NOT NULL,
-  `title` varchar(255) collate utf8_bin default NULL,
-  `uri` varchar(255) collate utf8_bin default NULL,
+  `title` varchar(255) default NULL,
+  `uri` varchar(255) default NULL,
   KEY `testcase_id` (`testcase_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -120,11 +121,11 @@ CREATE TABLE IF NOT EXISTS `testlinks` (
 --
 
 CREATE TABLE IF NOT EXISTS `testsequence` (
-  `engine` varchar(16) collate utf8_bin NOT NULL default '',
+  `engine` varchar(16) NOT NULL default '',
   `testcase_id` int(11) unsigned NOT NULL,
   `sequence` int(11) unsigned NOT NULL,
   PRIMARY KEY  (`engine`,`testcase_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -133,16 +134,16 @@ CREATE TABLE IF NOT EXISTS `testsequence` (
 --
 
 CREATE TABLE IF NOT EXISTS `testsuites` (
-  `testsuite` varchar(32) collate utf8_bin NOT NULL default '',
-  `base_uri` varchar(255) collate utf8_bin default NULL,
-  `home_uri` varchar(64) collate utf8_bin default NULL,
-  `spec_uri` varchar(255) collate utf8_bin default NULL,
-  `title` varchar(255) collate utf8_bin default NULL,
+  `testsuite` varchar(32) NOT NULL default '',
+  `base_uri` varchar(255) default NULL,
+  `home_uri` varchar(64) default NULL,
+  `spec_uri` varchar(255) default NULL,
+  `title` varchar(255) default NULL,
   `active` tinyint(1) NOT NULL default '1',
-  `description` longtext collate utf8_bin,
-  `sequence_query` varchar(32) collate utf8_bin default NULL,
+  `description` longtext,
+  `sequence_query` varchar(32) default NULL,
   PRIMARY KEY  (`testsuite`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -152,15 +153,11 @@ CREATE TABLE IF NOT EXISTS `testsuites` (
 
 CREATE TABLE IF NOT EXISTS `useragents` (
   `id` int(11) unsigned NOT NULL auto_increment,
-  `useragent` varchar(255) collate utf8_bin NOT NULL default '',
-  `engine` varchar(16) collate utf8_bin default NULL,
-  `engine_version` varchar(16) collate utf8_bin default NULL,
-  `browser` varchar(32) collate utf8_bin default NULL,
-  `browser_version` varchar(16) collate utf8_bin default NULL,
-  `platform` varchar(32) collate utf8_bin default NULL,
+  `useragent` varchar(255) NOT NULL default '',
+  `engine` varchar(16) default NULL,
+  `engine_version` varchar(16) default NULL,
+  `browser` varchar(32) default NULL,
+  `browser_version` varchar(16) default NULL,
+  `platform` varchar(32) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-
--- ---------------------------------------------------------
-
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
