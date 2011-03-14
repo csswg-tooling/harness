cb6683e298372431cd5e26d36ebd89b01157c427
plinss@d4b6719f-082f-4061-9c97-831ddc045d98 <plinss@d4b6719f-082f-4061-9c97-831ddc045d98>
2011-03-14 05:05:37 +0000

    correct utf-8 encoding

--- lib/DBResult.php
+++ lib/DBResult.php
@@ -1,9 +1,9 @@
 <?php
 /*******************************************************************************
  *
- *  Copyright © 2008-2011 Hewlett-Packard Development Company, L.P. 
+ *  Copyright Â© 2008-2011 Hewlett-Packard Development Company, L.P. 
  *
- *  This work is distributed under the W3C¨ Software License [1] 
+ *  This work is distributed under the W3CÂ® Software License [1] 
  *  in the hope that it will be useful, but WITHOUT ANY 
  *  WARRANTY; without even the implied warranty of 
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
@@ -11,7 +11,7 @@
  *  [1] http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231 
  *
  *  Adapted from the Mobile Test Harness
- *  Copyright © 2007 World Wide Web Consortium
+ *  Copyright Â© 2007 World Wide Web Consortium
  *  http://dev.w3.org/cvsweb/2007/mobile-test-harness/
  * 
  ******************************************************************************/
