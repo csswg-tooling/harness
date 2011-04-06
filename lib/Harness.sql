6361fe86e22546d94e7bf25e3fd06b638fabb510
plinss@d4b6719f-082f-4061-9c97-831ddc045d98 <plinss@d4b6719f-082f-4061-9c97-831ddc045d98>
2011-04-06 17:22:57 +0000

    capture db schema changes, fix line endings in format.php

--- lib/Harness.sql
+++ lib/Harness.sql
@@ -5,7 +5,6 @@
 
 -- --------------------------------------------------------
 
-
 --
 -- Table structure for table `flags`
 --
@@ -25,17 +24,15 @@ CREATE TABLE IF NOT EXISTS `flags` (
 --
 
 CREATE TABLE IF NOT EXISTS `formats` (
-  `format` enum('html4','xhtml1') NOT NULL default 'html4',
+  `format` enum('html4','xhtml1','xhtml1print') NOT NULL default 'html4',
   `title` varchar(255) NOT NULL,
+  `description` varchar(255) default NULL,
+  `home_uri` varchar(63) default NULL,
   `path` varchar(63) default NULL,
   `extension` varchar(15) default NULL,
   `filter` enum('HTMLonly','nonHTML') default NULL,
   PRIMARY KEY  (`format`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-INSERT INTO `formats` VALUES('html4', 'HTML 4', 'html4', 'htm', 'nonHTML');
-INSERT INTO `formats` VALUES('xhtml1', 'XHTML 1.1', 'xhtml1', 'xht', 'HTMLonly');
 
 -- --------------------------------------------------------
 
@@ -50,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `references` (
   `uri` varchar(255) NOT NULL,
   `type` enum('==','!=') NOT NULL,
   KEY `testcase_id` (`testcase_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -73,7 +70,21 @@ CREATE TABLE IF NOT EXISTS `results` (
   PRIMARY KEY  (`id`),
   KEY `useragent_id` (`useragent_id`),
   KEY `testcase_id` (`testcase_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8  ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `revisions`
+--
+
+CREATE TABLE IF NOT EXISTS `revisions` (
+  `testcase_id` int(11) unsigned NOT NULL default '0',
+  `revision` int(11) unsigned NOT NULL default '0',
+  `equal_revision` int(11) NOT NULL default '0',
+  `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  KEY `testcase_id` (`testcase_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -85,7 +96,38 @@ CREATE TABLE IF NOT EXISTS `sources` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `source` varchar(31) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8  ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specifications`
+--
+
+CREATE TABLE IF NOT EXISTS `specifications` (
+  `spec` varchar(31) NOT NULL,
+  `title` varchar(31) default NULL,
+  `description` varchar(255) default NULL,
+  `base_uri` varchar(255) default NULL,
+  `home_uri` varchar(63) default NULL,
+  PRIMARY KEY  (`spec`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `speclinks`
+--
+
+CREATE TABLE IF NOT EXISTS `speclinks` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `parent_id` int(11) unsigned NOT NULL default '0',
+  `spec` varchar(31) default NULL,
+  `section` varchar(31) default NULL,
+  `title` varchar(255) default NULL,
+  `uri` varchar(255) NOT NULL default '',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -102,6 +144,19 @@ CREATE TABLE IF NOT EXISTS `spidertrap` (
   `first_visit` timestamp NULL default NULL,
   `last_action` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`ip_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suitetests`
+--
+
+CREATE TABLE IF NOT EXISTS `suitetests` (
+  `testsuite` varchar(31) NOT NULL default '',
+  `testcase_id` int(11) unsigned NOT NULL default '0',
+  `revision` int(11) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`testsuite`,`testcase_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -124,58 +179,15 @@ CREATE TABLE IF NOT EXISTS `testcases` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `revisions`
---
-
-CREATE TABLE IF NOT EXISTS `revisions` (
-  `testcase_id` int(11) unsigned NOT NULL DEFAULT '0',
-  `revision` int(11) unsigned NOT NULL DEFAULT '0',
-  `equal_revision` int(11) NOT NULL DEFAULT '0',
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  KEY `testcase_id` (`testcase_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `suitetests`
---
-
-CREATE TABLE IF NOT EXISTS `suitetests` (
-  `testsuite` varchar(31) NOT NULL default '',
-  `testcase_id` int(11) unsigned NOT NULL default 0,
-  `revision` int(11) unsigned NOT NULL default 0,
-  PRIMARY KEY  (`testsuite`,`testcase_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `testlinks`
 --
+
 CREATE TABLE IF NOT EXISTS `testlinks` (
   `testcase_id` int(11) unsigned NOT NULL,
   `speclink_id` int(11) unsigned NOT NULL,
-  `group` tinyint(1) unsigned NOT NULL default 0,
-  PRIMARY KEY (`testcase_id`,`speclink_id`)
+  `group` tinyint(1) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`testcase_id`,`speclink_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `speclinks`
---
-
-CREATE TABLE IF NOT EXISTS `speclinks` (
-  `id` int(11) unsigned NOT NULL auto_increment,
-  `parent_id` int(11) unsigned NOT NULL default 0,
-  `spec` varchar(31) NULL,
-  `section` varchar(31) NULL,
-  `title` varchar(255) NULL,
-  `uri` varchar(255) NOT NULL default '',
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 -- --------------------------------------------------------
 
@@ -187,7 +199,7 @@ CREATE TABLE IF NOT EXISTS `testpages` (
   `testcase_id` int(11) unsigned NOT NULL,
   `format` enum('html4','xhtml1') NOT NULL default 'html4',
   `uri` varchar(255) NOT NULL default '',
-  PRIMARY KEY (`testcase_id`,`format`)
+  PRIMARY KEY  (`testcase_id`,`format`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -204,22 +216,6 @@ CREATE TABLE IF NOT EXISTS `testsequence` (
   PRIMARY KEY  (`testsuite`,`engine`,`testcase_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table `specifications`
---
-
-CREATE TABLE IF NOT EXISTS `specifications` (
-  `spec` varchar(31) NOT NULL,
-  `title` varchar(31) default NULL,
-  `description` varchar(255) default NULL,
-  `uri` varchar(255) default NULL,
-  PRIMARY KEY  (`spec`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
 -- --------------------------------------------------------
 
 --
@@ -235,23 +231,13 @@ CREATE TABLE IF NOT EXISTS `testsuites` (
   `formats` set('html4','xhtml1') NOT NULL default 'html4,xhtml1',
   `optional_flags` set('ahem','animated','combo','dom','font','history','http','HTMLonly','image','interact','invalid','namespace','nonHTML','may','may21','paged','reftest','should','scroll','svg','userstyle','32bit','96dpi') default NULL,
   `active` tinyint(1) unsigned NOT NULL default '1',
+  `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
   `locked` timestamp NULL default NULL,
   `description` longtext,
   `contact_name` varchar(63) default NULL,
   `contact_uri` varchar(255) default NULL,
   PRIMARY KEY  (`testsuite`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `testsuites`
---
-
-INSERT INTO `testsuites` VALUES('CSS21', 'http://test.csswg.org/suites/css2.1/nightly-unstable/', '', 'CSS21', 'CSS 2.1 Test Suite', 'html4,xhtml1', 1, 0, 'CSS 2.1 Test Suite, Nightly Build.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite');
-INSERT INTO `testsuites` VALUES('CSS21_RC1', 'http://test.csswg.org/suites/css2.1/20100917/', '', 'CSS21', 'CSS 2.1 Test Suite RC1', 'html4,xhtml1', 0, 0, 'CSS 2.1 Test Suite, Release Candidate 1.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite');
-INSERT INTO `testsuites` VALUES('CSS21_RC2', 'http://test.csswg.org/suites/css2.1/20101001/', '', 'CSS21', 'CSS 2.1 Test Suite RC2', 'html4,xhtml1', 0, 0, 'CSS 2.1 Test Suite, Release Candidate 2.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite');
-INSERT INTO `testsuites` VALUES('CSS21_RC3', 'http://test.csswg.org/suites/css2.1/20101027/', '', 'CSS21', 'CSS 2.1 Test Suite RC3', 'html4,xhtml1', 0, 0, 'CSS 2.1 Test Suite, Release Candidate 3.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite');
-INSERT INTO `testsuites` VALUES('CSS21_RC4', 'http://test.csswg.org/suites/css2.1/20101210/', '', 'CSS21', 'CSS 2.1 Test Suite RC4', 'html4,xhtml1', 0, 0, 'CSS 2.1 Test Suite, Release Candidate 4.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite');
-INSERT INTO `testsuites` VALUES('CSS21_RC5', 'http://test.csswg.org/suites/css2.1/20110111/', '', 'CSS21', 'CSS 2.1 Test Suite RC5', 'html4,xhtml1', 0, 0, 'CSS 2.1 Test Suite, Release Candidate 5.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite');
 
 -- --------------------------------------------------------
 
@@ -267,7 +253,29 @@ CREATE TABLE IF NOT EXISTS `useragents` (
   `browser` varchar(31) default NULL,
   `browser_version` varchar(15) default NULL,
   `platform` varchar(31) default NULL,
+  `platform_version` varchar(31) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8  ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 
+
+-- --------------------------------------------------------
+-- --------------------------------------------------------
+-- --------------------------------------------------------
+
+
+INSERT INTO `formats` (`format`, `title`, `description`, `home_uri`, `path`, `extension`, `filter`) VALUES
+('html4', 'HTML 4', 'HTML 4.01', 'html4/toc.html', 'html4', 'htm', 'nonHTML'),
+('xhtml1', 'XHTML 1.1', 'XHTML 1.1', 'xhtml1/toc.xht', 'xhtml1', 'xht', 'HTMLonly'),
+('xhtml1print', 'XHTML-Print', 'XHTML 1.1 for Printers', 'xhtml1print/toc.xht', 'xhtml1print', 'xht', 'HTMLonly');
+
+-- --------------------------------------------------------
+
+INSERT INTO `testsuites` (`testsuite`, `base_uri`, `home_uri`, `spec`, `title`, `formats`, `optional_flags`, `active`, `date`, `locked`, `description`, `contact_name`, `contact_uri`) VALUES
+('CSS21_DEV', 'http://test.csswg.org/suites/css2.1/nightly-unstable/', '', 'CSS21', 'CSS 2.1 Test Suite Development Version', 'html4,xhtml1', 'may,may21,should', 1, '2011-04-04 00:00:00', '2011-04-04 18:20:40', 'CSS 2.1 Test Suite, Nightly Build.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite'),
+('CSS21_RC1', 'http://test.csswg.org/suites/css2.1/20100917/', '', 'CSS21', 'CSS 2.1 Test Suite RC1', 'html4,xhtml1', 'may,may21,should', 0, '2010-09-17 00:00:00', '2010-10-19 17:13:21', 'CSS 2.1 Test Suite, Release Candidate 1.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite'),
+('CSS21_RC2', 'http://test.csswg.org/suites/css2.1/20101001/', '', 'CSS21', 'CSS 2.1 Test Suite RC2', 'html4,xhtml1', 'may,may21,should', 0, '2010-10-01 00:00:00', '2010-11-16 11:34:22', 'CSS 2.1 Test Suite, Release Candidate 2.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite'),
+('CSS21_RC3', 'http://test.csswg.org/suites/css2.1/20101027/', '', 'CSS21', 'CSS 2.1 Test Suite RC3', 'html4,xhtml1', 'may,may21,should', 0, '2010-10-27 00:00:00', '2010-12-11 04:58:55', 'CSS 2.1 Test Suite, Release Candidate 3.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite'),
+('CSS21_RC4', 'http://test.csswg.org/suites/css2.1/20101210/', '', 'CSS21', 'CSS 2.1 Test Suite RC4', 'html4,xhtml1', 'may,may21,should', 0, '2010-12-10 00:00:00', '2011-01-13 09:58:49', 'CSS 2.1 Test Suite, Release Candidate 4.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite'),
+('CSS21_RC5', 'http://test.csswg.org/suites/css2.1/20110111/', '', 'CSS21', 'CSS 2.1 Test Suite RC5', 'html4,xhtml1', 'may,may21,should', 0, '2011-01-11 00:00:00', '2011-02-04 17:05:44', 'CSS 2.1 Test Suite, Release Candidate 5.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite'),
+('CSS21_RC6', 'http://test.csswg.org/suites/css2.1/20110323/', '', 'CSS21', 'CSS 2.1 Test Suite RC6', 'html4,xhtml1', 'may,may21,should', 1, '2011-03-23 00:00:00', '2011-03-23 11:48:50', 'CSS 2.1 Test Suite, Release Candidate 6.', 'public-css-testsuite@w3.org', 'http://lists.w3.org/Archives/Public/public-css-testsuite');
