862b5c215e5df1f7bfcf164109bda221ba8f0a13
Peter Linss <w3c@linss.com>
2018-09-27 09:26:30 -0700

    add alt text for engine icons

--- annotate.js
+++ annotate.js
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once('lib/Harness.php');
 
@@ -8,24 +8,24 @@ header('Content-Type: application/javascript; charset=utf-8');
 ?>
 /*******************************************************************************
  *
- *  Copyright © 2011-2014 Hewlett-Packard Development Company, L.P. 
+ *  Copyright © 2011-2014 Hewlett-Packard Development Company, L.P.
  *
- *  This work is distributed under the W3C® Software License [1] 
- *  in the hope that it will be useful, but WITHOUT ANY 
- *  WARRANTY; without even the implied warranty of 
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  This work is distributed under the W3C® Software License [1]
+ *  in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- *  [1] http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231 
+ *  [1] http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231
  *
  *  Adapted from the Mobile Test Harness
  *  Copyright © 2007 World Wide Web Consortium
  *  http://dev.w3.org/cvsweb/2007/mobile-test-harness/
- * 
+ *
  ******************************************************************************/
 
 /**
   Data returned from server:
-  
+
   response.title;
   response.description;
   response.heading; *
@@ -38,12 +38,12 @@ header('Content-Type: application/javascript; charset=utf-8');
   response.is_index; *
   response.engine_titles = { _engineName: title, ... };
   response.sections = { _anchor: section_data, ... };
-  
+
   section_data.anchor_name; *
   section_data.section_name; *
   section_data.test_count;
   section_data.results = { _engineName: result_data, ... }
- 
+
   result_data.pass_count;
   result_data.fail_count;
 
@@ -62,14 +62,14 @@ var annotator = {
 
   mResponse: null,
   mClosed: false,
-  
+
   buildURI: function(base, sectionName) {
     if (sectionName) {
       return base + 'section/' + sectionName + '/';
     }
     return base;
   },
-  
+
   removeAnnotation: function(anchorName) {
     try {
       var annotation = document.getElementById('annotation_' + (anchorName ? anchorName : 'root_'));
@@ -81,7 +81,7 @@ var annotator = {
     catch (err) {
     }
   },
-  
+
   removeAllAnnotations: function () {
     try {
       if (this.mResponse && this.mResponse.sections) {
@@ -95,13 +95,13 @@ var annotator = {
     catch (err) {
     }
   },
-  
+
   toggleAnnotations: function() {
     this.mClosed = (! this.mClosed);
     this.removeAllAnnotations();
     this.addAnnotations();
   },
-  
+
   toggleDetails: function(domEvent) {
     var engineNode = domEvent.target;
     while (engineNode && ('SPAN' != engineNode.tagName.toUpperCase())) {
@@ -110,30 +110,30 @@ var annotator = {
     var engineName = engineNode.getAttribute('data-engineName');
     var annotation = engineNode.parentNode.parentNode;
     var details = annotation.lastChild;
-    
+
     if (engineName == details.getAttribute('data-engineName')) {
       details.setAttribute('class', 'details');
       details.removeAttribute('data-engineName');
     }
     else {
       details.setAttribute('data-engineName', engineName);
-      
+
       var engineTitle = this.mResponse.engine_titles[engineName];
       var section = annotation.getAttribute('data-section');
 
       var detailsEngine = details.firstChild;
       var detailsLink = detailsEngine.lastChild;
-     
+
       detailsEngine.firstChild.textContent = engineTitle + ' ';
       detailsLink.setAttribute('href', this.buildURI(this.mResponse.results_uri, section));
-      
+
       var meter = details.lastChild;
       var numbers = meter.firstChild;
       var passBar = numbers.nextSibling;
       var failBar = passBar.nextSibling;
       var needBar = failBar.nextSibling;
       numbers.textContent = engineNode.getAttribute('title');
-      
+
       var passCount = parseInt(engineNode.getAttribute('data-passCount'), 10);
       var failCount = parseInt(engineNode.getAttribute('data-failCount'), 10);
       var needCount = parseInt(engineNode.getAttribute('data-needCount'), 10);
@@ -142,18 +142,22 @@ var annotator = {
       passBar.setAttribute('style', 'width: ' + ((passCount / total) * 100.0) + '%');
       failBar.setAttribute('style', 'width: ' + ((failCount / total) * 100.0) + '%');
       needBar.setAttribute('style', 'width: ' + ((needCount / total) * 100.0) + '%');
-      
+
       details.setAttribute('class', 'details open');
     }
-    
+
   },
-  
+
+  capitalize(str) {
+    return (str[0].toUpperCase() + str.substr(1));
+  },
+
   addAnnotationTo: function(anchorElement, section, first) {
     try {
       var headings = {'h1':'', 'h2':'', 'h3':'', 'h4':'', 'h5':'', 'h6':'',
                       'H1':'', 'H2':'', 'H3':'', 'H4':'', 'H5':'', 'H6':''};
       var targetElement = anchorElement;
-      
+
       while (targetElement && (Node.ELEMENT_NODE == targetElement.nodeType) && (! (targetElement.tagName in headings))) {
         targetElement = targetElement.parentNode;
       }
@@ -196,7 +200,7 @@ var annotator = {
           disclosure.setAttribute('onclick', 'annotator.toggleAnnotations()');
           annotation.appendChild(disclosure);
         }
-        
+
         // close box
         var closeBox = document.createElement('div');
         closeBox.setAttribute('class', 'close button');
@@ -207,21 +211,21 @@ var annotator = {
           closeBox.setAttribute('onclick', 'annotator.removeAnnotation("' + section.anchor_name + '")');
         }
         annotation.appendChild(closeBox);
-        
+
         // Test suite info
         if ((! this.mClosed) && first && this.mResponse.is_index && this.mResponse.heading) {
           var title = document.createElement('div');
           title.setAttribute('class', 'title');
-          
+
           title.innerHTML = this.mResponse.heading;
-          
+
           annotation.appendChild(title);
         }
-        
+
         // Test count heading
         var heading = document.createElement('div');
         heading.setAttribute('class', 'heading');
-        
+
         var testLink = document.createElement('a');
         testLink.setAttribute('href', this.buildURI(this.mResponse.test_uri, section.section_name));
 
@@ -234,7 +238,7 @@ var annotator = {
         else {
           testLink.appendChild(document.createTextNode(section.test_count + ' Tests'));
         }
-        
+
         // Testing needed text
         if ((! this.mClosed) && (! this.mResponse.lock_date) && (0 < needCount)) {
           var untested = document.createElement('span');
@@ -261,12 +265,12 @@ var annotator = {
           var minorEngines = document.createElement('div');
           majorEngines.setAttribute('class', 'engines');
           minorEngines.setAttribute('class', 'engines');
-          
+
           for (engineName in section.results) {
             if (section.results.hasOwnProperty(engineName)) {
               var engineResults = section.results[engineName];
               var resultCount = (engineResults.pass_count + engineResults.fail_count);
-              
+
               var toolTip = '';
               var engineClass = '';
               if (0 < resultCount) {
@@ -315,7 +319,7 @@ var annotator = {
               else {
                 toolTip = 'No data';
               }
-              
+
               if (0 < resultCount) {
                 var engineNode = document.createElement('span');
                 engineNode.setAttribute('title', toolTip);
@@ -333,6 +337,7 @@ var annotator = {
                   engineClass += ' major';
                   var logo = document.createElement('img');
                   logo.setAttribute('src', this.ENGINE_LOGOS[engineName]);
+                  logo.setAttribute('alt', this.capitalize(engineName.substr(1)));
                   engineNode.appendChild(logo);
                   majorEngines.appendChild(engineNode);
                   majorEngines.appendChild(document.createTextNode(' '));
@@ -349,19 +354,19 @@ var annotator = {
           }
           annotation.appendChild(majorEngines);
           annotation.appendChild(minorEngines);
-          
+
           var details = document.createElement('div');
           details.setAttribute('class', 'details');
-          
+
           var engineName = document.createElement('div');
           engineName.setAttribute('class', 'engine');
           engineName.appendChild(document.createTextNode('engine '));
-          
+
           var detailsLink = document.createElement('a');
           detailsLink.appendChild(document.createTextNode('test details'));
           engineName.appendChild(detailsLink);
           details.appendChild(engineName);
-          
+
           var meter = document.createElement('div');
           meter.setAttribute('class', 'meter');
           for (barClass in { 'numbers': '', 'pass': '', 'epic-fail': '', 'untested': '' }) {
@@ -370,10 +375,10 @@ var annotator = {
             meter.appendChild(bar);
           }
           details.appendChild(meter);
-          
+
           annotation.appendChild(details);
         }
-        
+
         targetElement.insertBefore(annotation, targetElement.firstChild);
         return true;
       }
@@ -383,7 +388,7 @@ var annotator = {
     }
     return false;
   },
-  
+
   addAnnotation: function(section, first) {
     try {
       var anchorName = section.anchor_name;
@@ -394,7 +399,7 @@ var annotator = {
         anchor = document.getElementById(anchorName);
         if (! (anchor && this.addAnnotationTo(anchor, section, first))) {
           var anchors = document.getElementsByName(anchorName);
-          
+
           for (index in anchors) {
             var anchor = anchors[index];
             if (this.addAnnotationTo(anchor, section, first)) {
@@ -405,7 +410,7 @@ var annotator = {
       }
       else if (first) {  // find first h1
         var headings = document.getElementsByTagName('h1');
-        
+
         if (headings && (0 < headings.length)) {
           this.addAnnotationTo(headings[0], section, first);
         }
@@ -414,7 +419,7 @@ var annotator = {
     catch (err) {
     }
   },
-  
+
   addAnnotations: function () {
     try {
       if (this.mResponse && this.mResponse.sections) {
@@ -435,7 +440,7 @@ var annotator = {
     catch (err) {
     }
   },
-  
+
   processResponse: function(contentType, responseText) {
     try {
       if (-1 < contentType.indexOf('application/vnd.csswg.harness.v1+json')) {
@@ -450,11 +455,11 @@ var annotator = {
     catch (err) {
     }
   },
-  
+
   annotate: function() {
     try {
       var testSuiteName = '';
-      
+
       var scripts = document.getElementsByTagName('script');
       for (index in scripts) {
         if (scripts[index].hasAttribute('src')) {
@@ -469,14 +474,14 @@ var annotator = {
           }
         }
       }
-      
+
       if (0 < testSuiteName.length) {
         var styleSheet = document.createElement('link');
         styleSheet.setAttribute('rel', 'stylesheet');
         styleSheet.setAttribute('type', 'text/css');
         styleSheet.setAttribute('href', this.STYLESHEET_URI);
         document.getElementsByTagName('head')[0].appendChild(styleSheet)
-        
+
         var specName = '';
         var slashIndex = testSuiteName.indexOf('/');
         if (-1 < slashIndex) {
@@ -500,7 +505,7 @@ var annotator = {
         }
         else {  // The standard way
           var xhr = new XMLHttpRequest();
-          
+
           xhr.onreadystatechange = function() {
             if (4 == xhr.readyState) {
               if (200 == xhr.status) {
@@ -514,7 +519,7 @@ var annotator = {
               }
             }
           };
-          
+
           xhr.open('GET', statusURI, true);
           xhr.setRequestHeader('Accept', 'application/vnd.csswg.harness.v1+json');
           xhr.send();
@@ -525,7 +530,7 @@ var annotator = {
 //      document.body.innerHTML = 'EXCEPTION: ' + err.toString(); // DEBUG
     }
   },
-  
+
   addLoadEvent: function() {
     try {
       var oldOnLoad = window.onload;
